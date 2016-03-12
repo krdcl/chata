e288dfe6b3953dbf8ff8c385d37080bf73a78b6a
Samoenko Yuriy <krdcl@mail.ru>
2016-03-12 12:08:30 +0200

    //

--- app.js
+++ app.js
@@ -15,13 +15,16 @@ var fs = require('fs');
 var config = require('./config.json');
 var pack = require('./package.json');
 var path = require('path');
-
+var websocket  = require('ws');
 
 var port = 3000;
+var portWeb =  3001;
 var app = express();
 var server;
 
-
+var serverWS = new websocket.Server({
+    port: portWeb
+});
 
 
 /* Express */
@@ -52,4 +55,33 @@ function onListening() {
     var addr = server.address();
     var bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port;
     debug('start', 'Listening at ' + bind);
+}
+
+
+server.on("text", function (str)
+{
+    debug("Received "+str)
+    conn.sendText(str.toUpperCase()+"!!!")
+})
+
+server.on("close", function (code, reason)
+{
+    debug("Connection closed")
+    //delete clients[id];
+})
+
+server.on('message', function(message)
+{
+    debug ('получено сообщение ' + message);
+    /* for (var key in clients) {
+     clients[key].send(message);
+     }*/
+});
+
+var sendBroadcast = function broadcast(server, msg)
+{
+    server.connections.forEach(function (conn)
+    {
+        conn.sendText(msg);
+    })
 }
